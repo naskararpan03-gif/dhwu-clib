07e1fc0576eea39eef8eed7c92b6c8969532b0b2
naskararpan03-gif <naskararpan03@gmail.com>
2026-03-29 22:42:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-function openPopup(src) {
-  document.getElementById("popup").style.display = "flex";
-  document.getElementById("popup-img").src = src;
+function showSection(id) {
+    document.querySelectorAll("section").forEach(sec => sec.classList.remove("active"));
+    document.getElementById(id).classList.add("active");
 }
 
-function closePopup() {
-  document.getElementById("popup").style.display = "none";
+/* IMAGE POPUP */
+function openModal(src) {
+    let modal = new bootstrap.Modal(document.getElementById('imgModal'));
+    document.getElementById('modalImg').src = src;
+    modal.show();
 }
